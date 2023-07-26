83e81fc2f040cb1b4d2200518691ef3ef2d4dfd7
Felipe Aguirre <exemplo@teste.com.br>
2023-07-26 18:05:49 -0300

    first commit

---
+++ tarefa_wrapper/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int valor1 = 30;
+        System.out.println(valor1);
+
+        int valor2 = Integer.valueOf(valor1);
+        System.out.println(valor2);
+        }
+    }
